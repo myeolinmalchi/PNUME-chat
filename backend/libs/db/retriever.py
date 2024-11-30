7eac436fb5d0dc7eaa0cb36c029cd72f4a5a78ec
JakeFRCSE <eppi001004@gmail.com>
2024-11-30 16:46:33 +0900

    Deleted some codes for experiment. Note that this code is NOT working.

--- backend/libs/db/retriever.py
+++ backend/libs/db/retriever.py
@@ -4,8 +4,6 @@ import common
 import os
 from dotenv import load_dotenv
 
-import openai
-
 load_dotenv()
 
 db_type = "postgresql"
@@ -13,25 +11,7 @@ user = os.environ.get("DB_USER")
 pw = os.environ.get("DB_PASSWORD")
 host = os.environ.get("DB_HOST")
 dbname = os.environ.get("DB_NAME")
-openai_key = os.environ.get("OPENAI_KEY")
 
-
-# OpenAI API 키를 설정하세요
-openai.api_key = openai_key
-
-def get_embedding(text, model="text-embedding-3-small"):
-    response = openai.Embedding.create(
-        input=text,
-        model=model,
-        dimensions=common.N_DIM
-    )
-    embedding = response['data'][0]['embedding']
-    return embedding
-
-question = "무슨 교육 관련 공지가 있었는데 뭔지 기억이 안 나"
-question_vector = get_embedding(question)
-
-dictionary = {"any": "anything"}
 
 class Retriever:
     def __init__(self):
@@ -39,7 +19,7 @@ class Retriever:
         self.question_vector = []
         self.retrieved_noitces = []
         self.prompt = "다음은 게시물의 제목과 내용을 토대로 답변하세요. 질문에 대한 답변만 하고 이외의 필요없는 단어는 생성하지 마세요. 다음은 제목과 내용들입니다."
-        self.repository = NoticeRepository('notice', (1 ,2), dictionary)
+        self.repository = NoticeRepository()
 
     def total_similarity_retrieve(self, question:str, question_vector:list,) -> str:
         self.question = question
@@ -49,7 +29,3 @@ class Retriever:
             self.prompt += f"\n제목:{notice.title}\n내용:{notice.content}\n"
         self.prompt += f"\n질문:{self.question}"
         return self.prompt
-
-retriever = Retriever()
-prompt = retriever.total_similarity_retrieve(question=question, question_vector=question_vector)
-print(prompt)
