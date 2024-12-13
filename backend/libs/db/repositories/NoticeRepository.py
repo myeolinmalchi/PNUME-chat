3f85227d2402fc0f120cca7728a4b4fa2493768f
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-12-13 23:18:16 +0900

    feat: hybrid(dense + sparse) retriever 메소드 구현

--- backend/libs/db/repositories/NoticeRepository.py
+++ backend/libs/db/repositories/NoticeRepository.py
@@ -1,4 +1,6 @@
-from typing import List
+from typing import Dict, List, Optional
+
+from sqlalchemy import func
 from libs.db.models.Notice import Notice
 from libs.db.repositories.BaseRepository import BaseRepository
 
@@ -45,15 +47,48 @@ class NoticeRepository(BaseRepository[Notice]):
 
     def find_orderby_content_similarity(
         self, query_encoding: List, threshold: float = 0.7, k: int = 5
-    ):
+    ) -> List[Notice]:
         """내용으로 유사도 검색"""
-        similarity = Notice.content_vector.cosine_distance(query_encoding).label(
-            "similarity"
-        )
+        similarity = (
+            1 - func.cosine_distance(Notice.content_vector, query_encoding)
+        ).label("similarity")
         return (
-            self.session.query(Notice, similarity)
+            self.session.query(Notice)
             .filter(similarity > threshold)
-            .order_by("similarity")
+            .order_by(similarity.desc())
             .limit(k)
             .all()
         )
+
+    def search_orderby_weighted_sum(
+        self,
+        query_vector: Optional[List[float]] = None,
+        query_sparse_vector: Optional[List[Dict[str, float]]] = None,
+        lexical_ratio: float = 0.5,
+        title_ratio: float = 0.5,
+        k: int = 5,
+    ) -> List[Notice]:
+        score_title_dense = 1 - func.cosine_distance(Notice.title_vector, query_vector)
+        score_content_dense = 1 - func.cosine_distance(
+            Notice.content_vector, query_vector
+        )
+        score_title_lexical = -1 * (
+            func.inner_product(Notice.title_sparse_vector, query_sparse_vector)
+        )
+        score_content_lexical = -1 * (
+            func.inner_product(Notice.content_sparse_vector, query_sparse_vector)
+        )
+        score_title = (score_title_lexical * lexical_ratio) + score_title_dense * (
+            1 - lexical_ratio
+        )
+        score_content = (
+            score_content_lexical * lexical_ratio
+        ) + score_content_dense * (1 - lexical_ratio)
+
+        score = (
+            (score_title * title_ratio) + (score_content * (1 - title_ratio))
+        ).label("score")
+
+        query = self.session.query(Notice).order_by(score.desc()).limit(k)
+
+        return query.all()
