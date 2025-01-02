d3b4ef80b5009e426a24e4d9705a32fb83ba7372
박상훈 <sanghoon20020403@gmail.com>
2025-01-02 20:07:18 +0900

    temp

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -10,11 +10,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 ##data validation(pydantic)
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from pydantic import Field, field_validator
 
-##env
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
 app = FastAPI()
 
@@ -33,30 +31,36 @@ app.add_middleware(
 )
 
 ## bind DB
-## pydantic-settings.BaseSettings 써서 검증코드 추가
+## pydantic-settings.BaseSettings 써서 검증코드 
+class DBConfig(BaseSettings):
+    DB_USER: str = Field(env = "db_user")
+    DB_PASSWORD: str = Field(env = "db_password")
+    DB_HOST: str = Field(env = "db_host")
+    DB_PORT: int = Field(env = "db_port")
+    DB_DATABASE: str = Field(env = 'db_database')
+
+
+    class Config:
+        env_file = 'backend/.env'
+
+db_config = DBConfig()
+SQLALCHEMY_DB_URL = f"postgresql://{db_config.DB_USER}:{db_config.DB_PASSWORD}@{db_config.DB_HOST}:{db_config.DB_PORT}/{db_config.DB_DATABASE}"
+
+
+
+
 Base = declarative_base()
-
-DB_USER = os.environ.get('db_user')
-DB_PASSWORD = os.environ.get('db_password')
-DB_HOST = os.environ.get('db_host')
-DB_PORT = os.environ.get('db_port')
-DB_DATABASE = os.environ.get('db_database')
-SQLALCHEMY_DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
-
 engine = create_engine(SQLALCHEMY_DB_URL, echo=False)
 Base.metadata.bind = engine 
 
 
 ##pydantic써서 requeset data로 받을 데이터형 설정
 class Item(BaseModel):
-    quest: str
+    questionContents: str
 
 
 
 @app.post("/question")
-##원래 get api를 쓰고 query parameter에 사용자 질문 담아 보낼려고 했음. get api는 request body가 없으니깐 !!
-##근데 queyr-parameter는 대게 256byte 길이 제한이 있어서 사용자의 질문이 길어질 경우 제대로 된 응답이 힘들다. (웹 브라우저 마다 다르긴 함)
-##get이 아닌 post api쓰고 사용자 질문을 request body에 담아서 보내기러 함.
 async def question(item: Item):
     """
 #일단 비동기 처리 신경쓰지 말고 한번에 하나의 질문만 받고 답한다고 생각하고 먼저 코드 구현.
@@ -64,6 +68,6 @@ async def question(item: Item):
     2. 받은 질문을 응답 생성 모델에 집어 넣는다.
     3. 모델이 생성한 질문을 받아와서 프론트로 넘긴다. (return)
     """
-    quest = item.quest
+    
     #inference
     #return inference_result
