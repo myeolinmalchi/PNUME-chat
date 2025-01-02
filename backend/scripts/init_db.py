b042191259734e9019be6d0c90ad0d7704245f91
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-03 00:33:57 +0900

    refactor(scripts): `backend/init_db.py` -> `backend/scripts/init_db.py`

--- backend/scripts/init_db.py
+++ backend/scripts/init_db.py
@@ -1,31 +1,38 @@
-import os
-
-from sqlalchemy import inspect, create_engine, text
 from sqlalchemy.exc import ProgrammingError
-from libs.db.common import Base, get_engine
-from libs.db.models import Notice
+from db.common import Base, get_engine
+from db.models import *
+
 
 def init_database():
+    """
     db_type = "postgresql"
     user = os.environ.get("DB_USER")
     pw = os.environ.get("DB_PASSWORD")
     host = os.environ.get("DB_HOST")
     db_name = os.environ.get("DB_NAME")
 
-    engine = create_engine(f"{db_type}://{user}:{pw}@{host}")
+    engine = create_engine(f"{db_type}://{user}:{pw}@{host}/postgres")
+    """
 
     try:
+        """
         inspector = inspect(engine)
         databases = inspector.get_schema_names()
 
-        if db_name in databases:
-            with engine.connect() as conn:
+        with engine.connect() as conn:
+            conn.execute(text(f"SET AUTOCOMMIT = ON;"))
+            conn.commit()
+            if db_name in databases:
                 conn.execute(text(f"DROP DATABASE IF EXISTS {db_name}"))
-                conn.execute(text(f"CREATE DATABASE {db_name}"))
+            conn.execute(text(f"CREATE DATABASE {db_name}"))
+        """
 
         engine = get_engine()
+        """
         with engine.connect() as conn:
             conn.execute(text(f"CREATE EXTENSION IF NOT EXISTS vector"))
+            conn.commit()
+        """
 
         Base.metadata.create_all(engine)
     except ProgrammingError as e:
