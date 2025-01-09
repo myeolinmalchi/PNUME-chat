871e71e5b776f8c82f38753f3ec4d2fc48c1a10f
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-09 14:30:26 +0900

    feat: 공지 크롤링 스크립트 간소화

--- backend/scripts/crawler/notice_me.py
+++ backend/scripts/crawler/notice_me.py
@@ -34,8 +34,6 @@ async def run(interval: int, category: str):
 
     categories = URLs.keys() if category == "ALL" else [category]
 
-    from tqdm import tqdm
-
     for url_key in categories:
         try:
             seq = notice_crawler.fetch_last_seq(url_key)
@@ -48,28 +46,10 @@ async def run(interval: int, category: str):
             if seq is None:
                 raise Exception(f"마지막 게시글 번호를 불러오지 못했습니다.")
 
-            scrape_result = await notice_crawler.ascrape_all(url_key, interval, to=-1)
-            notices, title_embeddings, content_embeddings = scrape_result
-
-            logging.info(
-                f"게시글 수: {len(notices)}",
-                extra={
-                    "category": url_key,
-                },
-            )
-            notices = [notice for notice in notices if notice is not None]
-            content_lengths = [len(notice["content"]) for notice in notices]
-            max_length = max(content_lengths)
-            logging.info(
-                f"최대 본문 길이: {max_length}",
-                extra={
-                    "category": url_key,
-                },
-            )
-
-            notice_service.create_notices(
-                url_key, notices, title_embeddings, content_embeddings
-            )
+            notices = await notice_crawler.ascrape_all(url_key, interval)
+            notices = list(notices.values())
+            notices = [notice for notice in notices if "info" in notice]
+            notice_service.create_notices(url_key, notices)
         except Exception:
             logging.exception(
                 "Error while scraping",
