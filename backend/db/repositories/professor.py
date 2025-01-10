8e8ef73a8d6e5e10faca8056805aae7a6e5e0721
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-10 19:53:08 +0900

    feat: `ProfessorRepository` 추가

---
+++ backend/db/repositories/professor.py
@@ -0,0 +1,7 @@
+from db.models.professor import ProfessorModel
+from db.repositories.base import BaseRepository
+
+
+# TODO: 검색 기능 구현
+class ProfessorRepository(BaseRepository[ProfessorModel]):
+    pass
