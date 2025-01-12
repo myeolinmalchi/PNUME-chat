53b8e53794a747d85eccccd5b9b36b1b882e844b
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-13 05:08:16 +0900

    feat: session 주입을 위해 추상 메서드 분리

--- backend/services/base/embedder.py
+++ backend/services/base/embedder.py
@@ -9,10 +9,32 @@ DTO = TypeVar("DTO")
 
 
 class BaseEmbedder(Generic[DTO], metaclass=HTTPMetaclass):
-    @abstractmethod
-    async def embed_async(self, item: D, sess: ClientSession) -> D:
-        raise NotImplementedError("method 'aembed' must be implemented before use.")
+    async def embed_dto_async(
+        self, item: DTO, session: Optional[ClientSession] = None
+    ) -> DTO:
+        if session is None:
+            raise ValueError("parameter 'session' cannot be None.")
+
+        return await self._embed_dto_async(item, session=session)
 
     @abstractmethod
-    async def embed_batch_async(self, items: List[D], timeout: int = 60) -> List[D]:
-        raise NotImplementedError("method 'aembed_batch' must be implemented before use.")
+    async def _embed_dto_async(self, item: DTO, session: ClientSession) -> DTO:
+        pass
+
+    async def embed_all_async(
+        self,
+        items: List[DTO],
+        interval: int = 30,
+        session: Optional[ClientSession] = None,
+    ) -> List[DTO]:
+        if session is None:
+            raise ValueError("parameter 'session' cannot be None.")
+
+        return await self._embed_all_async(items, interval, session=session)
+
+    @abstractmethod
+    async def _embed_all_async(
+        self, items: List[DTO], interval: int, session: ClientSession
+    ) -> List[DTO]:
+        pass
+
