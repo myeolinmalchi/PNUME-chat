31f4d29040d1f469aea97542aec7d91bbdb23cc4
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-13 05:11:32 +0900

    feat: 크롤링 및 임베딩 로직 개편

--- backend/services/notice_me/crawler.py
+++ backend/services/notice_me/crawler.py
@@ -1,150 +1,80 @@
 import asyncio
-from typing import Dict, Optional
+from typing import List, Optional
 
-import aiohttp
 from bs4 import BeautifulSoup
 from markdownify import markdownify as md
 from tqdm import tqdm
 
-from utils.scrape import scrape, ascrape_all
-from utils.embed import aembed_onnx
+from services.base.crawler import BaseCrawler
 
-from .dto import NoticeMEDTO, NoticeMEInfoDTO
+from .dto import NoticeMEDTO
 
 import re
 
-
 URLs = {
-    "공지/학부": {"path": "/new/sub05/sub01_01.asp", "db": "hakbunotice"},
-    "공지/대학원": {"path": "/new/sub05/sub01_02.asp", "db": "gradnotice"},
-    "공지/장학": {"path": "/new/sub05/sub01_05.asp", "db": "supervision"},
-    "공지/홍보": {"path": "/new/sub05/sub01_03.asp", "db": "notice"},
-    # "공지/기자재": {
-    #     "path": "/new/sub05/sub01_04.asp",
-    #     "db": ""
-    # },
-    "학부 소식": {"path": "/new/sub05/sub_02.asp", "db": "hakbunews"},
-    "언론 속 학부": {"path": "/new/sub05/sub03.asp", "db": "media"},
-    "세미나": {"path": "/new/sub05/sub04.asp", "db": "seminar"},
-    # "자유게시판": {
-    #     "path": "/new/sub06/sub05.asp",
-    #     "db": ""
-    # },
-    "취업정보": {"path": "/new/sub05/sub05.asp", "db": "recruit"},
+    "공지/학부": {
+        "path": "/new/sub05/sub01_01.asp",
+        "db": "hakbunotice"
+    },
+    "공지/대학원": {
+        "path": "/new/sub05/sub01_02.asp",
+        "db": "gradnotice"
+    },
+    "공지/장학": {
+        "path": "/new/sub05/sub01_05.asp",
+        "db": "supervision"
+    },
+    "공지/홍보": {
+        "path": "/new/sub05/sub01_03.asp",
+        "db": "notice"
+    },
+    "학부_소식": {
+        "path": "/new/sub05/sub_02.asp",
+        "db": "hakbunews"
+    },
+    "언론_속_학부": {
+        "path": "/new/sub05/sub03.asp",
+        "db": "media"
+    },
+    "세미나": {
+        "path": "/new/sub05/sub04.asp",
+        "db": "seminar"
+    },
+    "취업정보": {
+        "path": "/new/sub05/sub05.asp",
+        "db": "recruit"
+    },
 }
 
 SELECTORs = {
     "list": "#contents > div > div > div > div.board-list02 > table > tbody > tr",
     "detail": {
-        "title": "#contents > div > div > div.board-view > dl:nth-child(1) > dd",
-        "date": "#contents > div > div > div.board-view > dl:nth-child(2) > dd",
-        "author": "#contents > div > div > div.board-view > dl:nth-child(3) > dd",
+        "info": {
+            "title": "#contents > div > div > div.board-view > dl:nth-child(1) > dd",
+            "date": "#contents > div > div > div.board-view > dl:nth-child(2) > dd",
+            "author": "#contents > div > div > div.board-view > dl:nth-child(3) > dd",
+            "content": "#contents > div > div > div.board-contents.clear",
+        },
         "attachments": "#contents > div > div > div.board-view > dl.half-box01.none > dd",
-        "content": "#contents > div > div > div.board-contents.clear",
     },
 }
 
 
-class NoticeMECrawler:
-    def __init__(self):
-        pass
+class NoticeMECrawler(BaseCrawler[NoticeMEDTO]):
 
-    def fetch_last_seq(self, url_key: str) -> Optional[int]:
+    def scrape_last_seq(self, url_key: str) -> int:
         """가장 최근 게시글 seq 반환"""
         if url_key not in URLs.keys():
             raise Exception("최근 게시글 seq를 불러오지 못했습니다.")
 
-        soup = scrape(URLs[url_key]["path"])
-        return None if soup is None else self._parse_last_seq(soup)
+        soup = self._scrape(URLs[url_key]["path"])
+        seq = self._parse_last_seq(soup)
 
-    async def afetch_details(
-        self, url_key: str, st_seq: int, ed_seq: int
-    ) -> Dict[int, NoticeMEDTO]:
-        """게시글 세부 정보 비동기 추출"""
-        if url_key not in URLs:
-            raise Exception("존재하지 않는 URL입니다.")
-        if ed_seq < st_seq:
-            raise Exception("'ed_seq' must be greater than or equal to 'st_seq'")
-
-        path = URLs[url_key]["path"]
-        db = URLs[url_key]["db"]
-
-        _urls = [
-            f"{path}?db={db}&seq={seq}&page_mode=view"
-            for seq in range(st_seq, ed_seq + 1)
-        ]
-
-        soups = await ascrape_all(_urls, 30)
-        soups = [soup for soup in soups if soup is not None]
-        loop = asyncio.get_running_loop()
-        tasks = [
-            loop.run_in_executor(None, self._parse_detail, seq, soup)
-            for seq, soup in enumerate(soups, st_seq)
-        ]
-        notices = await asyncio.gather(*tasks)
-        notices = {notice["seq"]: notice for notice in notices}
-
-        return notices
-
-    async def aembed_notice(
-        self, notices: Dict[int, NoticeMEDTO]
-    ) -> Dict[int, NoticeMEDTO]:
-        infos = [notice["info"] for notice in notices.values() if "info" in notice]
-        titles = [info["title"] for info in infos]
-        contents = [info["content"] for info in infos]
-
-        timeout = aiohttp.ClientTimeout(total=3000)
-        async with aiohttp.ClientSession(timeout=timeout) as sess:
-            titles_coroutine = aembed_onnx(
-                session=sess,
-                texts=titles,
-                chunking=False,
-                truncate=True,
-            )
-            contents_coroutine = aembed_onnx(
-                session=sess,
-                texts=contents,
-                chunking=True,
-                truncate=False,
-            )
-
-            embeddings = await asyncio.gather(titles_coroutine, contents_coroutine)
-            for seq, title_vector, content_vector in zip(
-                notices.keys(), embeddings[0], embeddings[1]
-            ):
-                notices[seq]["embeddings"] = {
-                    "title_embeddings": title_vector,
-                    "content_embeddings": content_vector,
-                }
-
-            return notices
-
-    async def ascrape_all(
-        self, url_key: str, interval: int, to: int = -1
-    ) -> Dict[int, NoticeMEDTO]:
-        seq = self.fetch_last_seq(url_key)
-        if seq is None:
-            raise Exception(f"가장 최근 게시물을 불러오지 못했습니다({url_key})")
-
-        results: Dict[int, NoticeMEDTO] = {}
-
-        to = seq - interval if to == -1 else to
-        pbar = tqdm(
-            range(seq, to, interval * -1),
-            desc="Scraping",
-        )
-        for ed in pbar:
-            st = ed - interval
-            notices = await self.afetch_details(url_key, st, ed)
-            notices = await self.aembed_notice(notices)
-            results = {**results, **notices}
-
-        return results
+        return seq
 
     def _parse_last_seq(self, soup: BeautifulSoup):
         table_rows = soup.select(SELECTORs["list"])
 
-        seq: Optional[int] = None
         for row in table_rows:
             if row.has_attr("class") and row["class"][0] == "notice":
                 continue
@@ -158,63 +88,94 @@ class NoticeMECrawler:
 
             if seq_str != None:
                 seq = int(seq_str.group(1))
-                break
+                return seq
 
-        return seq
+        raise Exception
 
-    def _parse_detail(self, seq: int, soup: BeautifulSoup) -> NoticeMEDTO:
-        info: NoticeMEInfoDTO = {
-            "title": "",
-            "date": "",
-            "author": "",
-            "attachments": [],
-            "content": "",
-        }
+    async def _scrape_partial_async(self, seqs, session, **kwargs):
+        url_key = kwargs.get('url_key')
+        if url_key not in URLs:
+            raise Exception(f"존재하지 않는 카테고리입니다.({url_key})")
 
-        elements = {
-            key: soup.select_one(selector)
-            for key, selector in SELECTORs["detail"].items()
-        }
+        path = URLs[url_key]["path"]
+        db = URLs[url_key]["db"]
 
-        if any(element is None for _, element in elements.items()):
-            return {"seq": seq}
+        _urls = [f"{path}?db={db}&seq={seq}&page_mode=view" for seq in seqs]
 
-        # 타입 체크를 위한 멋진 코드
-        assert elements["title"] is not None
-        assert elements["date"] is not None
-        assert elements["author"] is not None
-        assert elements["attachments"] is not None
-        assert elements["content"] is not None
-
-        info["title"] = elements["title"].get_text(separator="", strip=True)
-        info["date"] = elements["date"].get_text(separator="", strip=True)
-        info["author"] = elements["author"].get_text(separator="", strip=True)
-        info["attachments"] = [
-            {
-                "name": self._preprocess_text(a.text, True),
-                "url": self._preprocess_text(str(a["href"]), True),
-            }
-            for a in elements["attachments"].select("a")
+        soups = await self._scrape_async(_urls, session=session)
+        loop = asyncio.get_running_loop()
+        tasks = [
+            loop.run_in_executor(None, self._parse_detail, seq, soup, url_key)
+            for seq, soup in zip(seqs, soups)
         ]
-        info["content"] = self._preprocess_text(
-            md(elements["content"].decode_contents())
+        notices = await asyncio.gather(*tasks)
+        notices = [notice for notice in notices if notice]
+
+        return notices
+
+    async def _scrape_all_async(self, interval, delay, session, **kwargs):
+        url_key = kwargs.get('url_key')
+        if not url_key or url_key not in URLs:
+            raise ValueError("존재하지 않는 카테고리입니다.")
+
+        seq = self.scrape_last_seq(url_key)
+        results: List[NoticeMEDTO] = []
+
+        to = kwargs.get('to', 1)
+        to = seq - interval if to == -1 else to
+        pbar = tqdm(
+            range(seq, to, interval * -1),
+            total=seq - to,
+            desc=f"Scraping Notices",
         )
+        for ed in pbar:
+            st = ed - interval
+            notices = await self.scrape_partial_async(
+                list(range(st, ed)), session=session, url_key=url_key
+            )
+            results += notices
+
+            pbar.update(interval)
+
+            pbar.set_postfix({"range": f"({st}-{ed})"})
+            await asyncio.sleep(delay)
+
+        return results
+
+    def _parse_detail(self, seq: int, soup: BeautifulSoup,
+                      category: str) -> Optional[NoticeMEDTO]:
+        _dto = {}
 
         for img in soup.select("img"):
             img.extract()
 
-        result: NoticeMEDTO = {"seq": seq, "info": info}
-        return result
+        elements = {
+            key: soup.select_one(selector)
+            for key, selector in SELECTORs["detail"]["info"].items()
+        }
 
-    def _preprocess_text(self, text: str, remove_newline: bool = False):
-        """텍스트 전처리"""
-        text = re.sub(r"\\+", "", text)  # 백슬래시(\\) 제거
-        text = re.sub(r"\n+", "\n", text)  # 여러 줄바꿈을 하나의 공백으로 대체
-        text = re.sub(r"\t", "", text)  # 여러 줄바꿈을 하나의 공백으로 대체
-        text = re.sub(r"\r", "", text)  # 여러 줄바꿈을 하나의 공백으로 대체
-        exclude_base64 = re.compile(r"data:image/[a-zA-Z]+;base64,[^\"']+")
-        text = re.sub(exclude_base64, "", text)
-        if remove_newline:
-            text = re.sub(r"\n", "", text)
-        re.sub(r"\s+", " ", text).strip()
-        return text
+        _dto["info"] = {
+            key: element.get_text(separator="", strip=True)
+            for key, element in elements.items() if element is not None
+        }
+
+        if "content" not in _dto["info"] or "title" not in _dto["info"]:
+            return None
+
+        att_element = soup.select_one(SELECTORs["detail"]["attachments"])
+
+        if att_element is not None:
+            _dto["attachments"] = [{
+                "name": self._preprocess_text(a.text, True),
+                "url": self._preprocess_text(str(a["href"]), True),
+            } for a in att_element.select("a")]
+
+        _dto["info"]["content"] = self._preprocess_text(
+            md(_dto["info"]["content"])
+        )
+
+        _dto["info"]["category"] = category
+
+        result = NoticeMEDTO(**_dto, seq=seq)
+
+        return result
