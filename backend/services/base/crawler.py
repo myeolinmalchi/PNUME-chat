ede4acd048e98c1eba85249e0186856b5dd48852
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-13 05:07:31 +0900

    feat(refactor): 크롤링 관련 공통 로직 이동

--- backend/services/base/crawler.py
+++ backend/services/base/crawler.py
@@ -1,8 +1,20 @@
 from abc import abstractmethod
-from typing import Generic, List, TypeVar
+import asyncio
+from typing import Generic, List, Optional, TypeVar, overload
 import re
 
+from aiohttp import ClientSession
+import aiohttp
+
+from mixins.asyncio import retry_async
+from mixins.http_client import HTTPMetaclass
+from bs4 import BeautifulSoup
+import requests
+
 DTO = TypeVar("DTO")
+
+domain = "https://me.pusan.ac.kr"
+
 
 class BaseCrawler(Generic[DTO], metaclass=HTTPMetaclass):
 
@@ -42,6 +54,74 @@ class BaseCrawler(Generic[DTO], metaclass=HTTPMetaclass):
         self, seqs: List[int], session: ClientSession, **kwargs
     ) -> List[DTO]:
         pass
+
+    @overload
+    async def _scrape_async(
+        self, path: str, session: ClientSession
+    ) -> BeautifulSoup:
+        ...
+
+    @overload
+    async def _scrape_async(self, path: List[str],
+                            session: ClientSession) -> List[BeautifulSoup]:
+        ...
+
+    async def _scrape_async(
+        self, path: str | List[str], session: ClientSession
+    ) -> BeautifulSoup | List[BeautifulSoup]:
+
+        @retry_async(delay=5)
+        async def scrape_coroutine(_path):
+            test = len(_path) == 0 or _path.startswith("/")
+            _path = _path if test else f"/{_path}"
+            async with session.get(f"{domain}{_path}") as res:
+                if res.status == 200:
+                    html = await res.read()
+                    soup = BeautifulSoup(html, "html.parser")
+                    return soup
+
+                raise aiohttp.ClientError
+
+        if isinstance(path, str):
+            return await scrape_coroutine(path)
+        return await asyncio.gather(
+            *[scrape_coroutine(_path) for _path in path]
+        )
+
+    @overload
+    def _scrape(self, path: str) -> BeautifulSoup:
+        ...
+
+    @overload
+    def _scrape(
+        self,
+        path: List[str],
+    ) -> List[BeautifulSoup]:
+        ...
+
+    def _scrape(self, path: str | List[str]):
+
+        def scrape(_path):
+            test = len(path) == 0 or _path.startswith("/")
+            _path = _path if test else f"/{_path}"
+
+            response = requests.get(f"{domain}{_path}", timeout=3.0)
+            if response.status_code == 200:
+                html = response.text
+                soup = BeautifulSoup(html, "html.parser")
+                return soup
+
+            raise Exception
+
+        if isinstance(path, str):
+            return scrape(path)
+
+        soups = []
+        for _path in path:
+            result = scrape(_path)
+            soups.append(result)
+
+        return soups
 
     def _preprocess_text(self, text: str, remove_newline: bool = False):
         """텍스트 전처리"""
