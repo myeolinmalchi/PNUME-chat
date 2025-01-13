875cedc80df37733ac5b93e3899056cd2fc96d47
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-13 22:43:38 +0900

    feat: `BaseEmbedder` 추상 메서드 구현 및 내부 로직 일부 수정

--- backend/services/professor/embedder.py
+++ backend/services/professor/embedder.py
@@ -1,39 +1,53 @@
-import asyncio
 from typing import List
 
-from aiohttp import ClientSession, ClientTimeout
 from services.base import BaseEmbedder
 from services.professor.dto import ProfessorDTO
-from utils.embed import aembed_onnx
+import asyncio
+
+
 class ProfessorMEEmbedder(BaseEmbedder[ProfessorDTO]):
 
+    async def _embed_all_async(self, items, interval, session):
 
-    async def aembed(self, item, sess):
-        from itertools import islice
+        async def embed_partial_async(item):
+            from itertools import islice
 
-        inputs: List[str] = []
-        lens: List[int] = []
+            inputs: List[str] = []
+            lens: List[int] = []
 
-        append = lambda acc, lens, xs: (acc + xs, lens + [len(xs)])
-        for k in ["fields", "educations", "careers"]:
-            if k in item:
-                inputs, lens = append(inputs, lens, [f["name"] for f in item[k]])
+            append = lambda acc, lens, xs: (acc + xs, lens + [len(xs)])
+            for k in ["fields", "educations", "careers"]:
+                if k in item["additional_info"]:
+                    inputs, lens = append(
+                        inputs, lens,
+                        [f["name"] for f in item["additional_info"][k]]
+                    )
 
-        embeddings = await aembed_onnx(texts=inputs, session=sess, chunking=False)
+            embeddings = await self._embed_async(
+                texts=inputs, session=session, chunking=False
+            )
 
-        iterator = iter(embeddings)
-        embeddings = [list(islice(iterator, length)) for length in lens]
+            iterator = iter(embeddings)
+            embeddings = [list(islice(iterator, length)) for length in lens]
 
-        for e, k in zip(embeddings, ["fields", "educations", "careers"]):
-            if k in item:
-                item[k] = [{**p, "embeddings": e} for p, e in zip(item[k], e)]
+            for e, k in zip(embeddings, ["fields", "educations", "careers"]):
+                if k in item["additional_info"]:
+                    item["additional_info"][k] = [{
+                        **p, "embeddings": e
+                    } for p, e in zip(item["additional_info"][k], e)]
 
-        return item
+            return item
 
-    async def aembed_batch(self, items, timeout=60):
-        timeout = ClientTimeout(total=timeout)
-        async with ClientSession(timeout=timeout) as sess:
-            coroutines = [self.aembed(p, sess) for p in items]
-            professors = await asyncio.gather(*coroutines)
+        def parts(_list, n):
+            for idx in range(0, len(_list), n):
+                yield _list[idx:idx + n]
+
+        parted_professors = list(parts(items, interval))
+
+        professors: List[ProfessorDTO] = []
+        for _professors in parted_professors:
+            professors += await asyncio.gather(
+                *[embed_partial_async(p) for p in _professors]
+            )
 
         return professors
