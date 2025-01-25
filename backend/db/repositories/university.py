fa7b0b1c63b09eee63b085d35a9636b2cefc884c
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-26 00:35:56 +0900

    feat: add `find_major` method
    - 교수님 크롤링 과정에서 세부 전공 유무 확인에 필요

--- backend/db/repositories/university.py
+++ backend/db/repositories/university.py
@@ -13,3 +13,26 @@ class UniversityRepository(BaseRepository[UniversityModel]):
             raise ValueError(f"존재하지 않는 학과입니다. ({name})")
 
         return result
+
+    def find_major(self, department: str, name: str):
+        department_model = self.session.query(DepartmentModel).filter(
+            DepartmentModel.name == department
+        ).first()
+
+        if not department_model:
+            raise ValueError(f"존재하지 않는 학과입니다. ({department})")
+
+        major_model = self.session.query(MajorModel).filter(
+            MajorModel.name == name
+        ).first()
+
+        if not major_model:
+            major_model = MajorModel(
+                name=name, department_id=department_model.id
+            )
+
+            self.session.add(major_model)
+            self.session.flush()
+            self.session.refresh(major_model)
+
+        return major_model
