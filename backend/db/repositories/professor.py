20b3c78e906596ebce7d7caaa7be6850d6ab27f4
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-01-26 00:34:46 +0900

    feat: add `delete_by_department` method
    - 학과 교수님 초기화. 크롤링 과정에 필요함

--- backend/db/repositories/professor.py
+++ backend/db/repositories/professor.py
@@ -1,9 +1,19 @@
 from db.models.professor import ProfessorModel
+from db.models.universities import DepartmentModel
 from db.repositories.base import BaseRepository
 
 
 class ProfessorRepository(BaseRepository[ProfessorModel]):
 
+    def delete_by_department(self, department: str):
+        department = self.session.query(DepartmentModel).filter(
+            DepartmentModel.name == department
         ).one_or_none()
+        if department is None:
+            raise ValueError(f"존재하지 않는 학과입니다: {department}")
 
+        affected = self.session.query(ProfessorModel).filter(
+            ProfessorModel.department_id == department.id
+        ).delete()
 
+        return affected
