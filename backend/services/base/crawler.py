118a0b6b4873ea9d059edfda9fe21629424450f0
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-02-01 01:26:55 +0900

    feat: 전처리 로직 개선

--- backend/services/base/crawler.py
+++ backend/services/base/crawler.py
@@ -103,15 +103,13 @@ class BaseCrawler(Generic[DTO], metaclass=HTTPMetaclass):
 
         return soups
 
-    def _preprocess_text(self, text: str, remove_newline: bool = False):
+    def _preprocess_text(self, text: str):
         """텍스트 전처리"""
-        text = re.sub(r"\\+", "", text)   # 백슬래시(\\) 제거
-        text = re.sub(r"\n+", "\n", text) # 여러 줄바꿈을 하나의 공백으로 대체
-        text = re.sub(r"\t", "", text)    # 여러 줄바꿈을 하나의 공백으로 대체
-        text = re.sub(r"\r", "", text)    # 여러 줄바꿈을 하나의 공백으로 대체
+        text = re.sub(r"\\+", " ", text)
+        text = re.sub(r"\n+", "\n", text)
+        text = re.sub(r"\t", " ", text)
+        text = re.sub(r"\r", " ", text)
         exclude_base64 = re.compile(r"data:image/[a-zA-Z]+;base64,[^\"']+")
         text = re.sub(exclude_base64, "", text)
-        if remove_newline:
-            text = re.sub(r"\n", "", text)
         re.sub(r"\s+", " ", text).strip()
         return text
