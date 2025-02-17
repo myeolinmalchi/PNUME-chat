4f1912f5e536cd45e67b4755d7d24cff8ed7a294
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-02-17 13:56:54 +0900

    feat: update `create_notice_service`

--- backend/services/notice/__init__.py
+++ backend/services/notice/__init__.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from .dto import *
 from .service import *
 from .embedder import *
@@ -9,7 +10,7 @@ def create_notice_service(_type: Literal["default", "me"] = "default"):
     notice_repo = NoticeRepository()
     notice_embedder = NoticeEmbedder()
     univ_repo = UniversityRepository()
-    notice_crawler = create_notice_crawler(_type)
+    notice_crawler = NoticeCrawler()
 
     match _type:
         case "default":
@@ -17,7 +18,7 @@ def create_notice_service(_type: Literal["default", "me"] = "default"):
         case "me":
             Service = NoticeMEService
 
-    notice_service = Service(
+    notice_service = NoticeService(
         notice_repo=notice_repo,
         notice_embedder=notice_embedder,
         notice_crawler=notice_crawler,
