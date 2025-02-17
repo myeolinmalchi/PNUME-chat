bbe9a5384a5bf789d13a78e4f1c1f8a4590cd3e1
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-02-17 13:52:32 +0900

    feat: (공통) 공지사항 크롤링 로직 개선

--- backend/services/notice/service/default.py
+++ backend/services/notice/service/default.py
@@ -1,14 +1,18 @@
+from typing import List, cast
 from tqdm import tqdm
 from urllib3.util import parse_url
 from config.config import get_notice_urls
+from db.models.notice import NoticeModel
 from db.repositories import transaction
 
 import asyncio
-from services.notice.dto import NoticeDTO
+from services.notice.dto import NoticeDTO, NoticeInfoDTO
 from services.notice.service.base import NoticeServiceBase
+from config.logger import _logger
+
 import logging
 
-logger = logging.getLogger(__name__)
+logger = _logger(__name__)
 
 
 class NoticeService(NoticeServiceBase):
@@ -17,94 +21,71 @@ class NoticeService(NoticeServiceBase):
     async def run_crawling_pipeline(self, **kwargs):
         department = kwargs.get("department")
         if not department:
-            raise ValueError
-
-        models = []
+            raise ValueError("'department' must be provided")
 
         url_dict = get_notice_urls(department)
 
         reset = kwargs.get("reset", False)
-        if reset:
-            affected = self.notice_repo.delete_by_department(department)
-            logger.info(f"[{department}] {affected} rows deleted.")
-
         interval = kwargs.get('interval', 30)
         rows = kwargs.get('rows', 500)
 
         if interval > rows:
             interval = rows
 
-        try:
-            for category, url in url_dict.items():
-                if not reset:
-                    last_notice = self.notice_repo.find_last_notice(
-                        departments=[department], categories=[category]
-                    )
-                    if last_notice:
-                        _last_url = parse_url(last_notice.url)
-                        _last_path = _last_url.path
-                        if not _last_path:
-                            raise ValueError(f"잘못된 url입니다: {_last_url.url}")
+        models: List[NoticeModel] = []
 
-                        _last_id = int(_last_path.split("/")[4])
-                        urls = self.notice_crawler.scrape_urls(
-                            url=url,
-                            rows=rows,
-                            **({
-                                "last_id": _last_id
-                            } if last_notice else {})
-                        )
-                    else:
-                        urls = self.notice_crawler.scrape_urls(
-                            url=url, rows=rows
-                        )
+        for category, url in url_dict.items():
+            try:
+                search_filter = {"departments": [department], "categories": [category]}
+
+                last_id = None
+                if reset:
+                    affected = self.notice_repo.delete_all(**search_filter)
+                    logger(f"[{department}-{category}] {affected} rows deleted.")
+
                 else:
-                    urls = self.notice_crawler.scrape_urls(url=url, rows=rows)
+                    last_notice = self.notice_repo.find_last_notice(**search_filter)
+                    if last_notice:
+                        last_path = parse_url(last_notice.url).path
+                        if not last_path:
+                            raise ValueError(f"잘못된 url입니다: {last_notice.url}")
+                        last_id = int(last_path.split("/")[4])
 
-                pbar = tqdm(
-                    range(0, len(urls), interval),
-                    total=len(urls),
-                    desc=f"[{department}-{category}]"
+                urls = await self.notice_crawler.scrape_urls_async(
+                    url=url, rows=rows, last_id=last_id
                 )
 
-                for st in pbar:
-                    ed = min(st + interval, len(urls))
-                    pbar.set_postfix({
-                        'range': f"{st + 1} ~ {ed}"
-                    })
+                with tqdm(total=len(urls), desc=f"[{department}-{category}]") as pbar:
+                    for st in range(0, len(urls), interval):
+                        ed = min(st + interval, len(urls))
+                        pbar.set_postfix({'range': f"{st + 1}-{ed}"})
 
-                    _urls = urls[st:ed]
-                    dtos = [
-                        NoticeDTO(
-                            **{
-                                "url": url,
-                                "info": {
-                                    "department": department,
-                                    "category": category
-                                }
-                            }
-                        ) for url in _urls
-                    ]
-                    notices = await self.notice_crawler.scrape_detail_async(
-                        dtos
-                    )
-                    notices = await self.notice_embedder.embed_dtos_batch_async(
-                        items=notices, batch_size=interval
-                    )
+                        def add_info(notice: NoticeDTO, **kwargs) -> NoticeDTO:
+                            info = notice["info"]
+                            info = cast(NoticeInfoDTO, {**info, **kwargs})
+                            return {**notice, "info": info}
 
-                    notice_models = [self.dto2orm(n) for n in notices]
-                    notice_models = [n for n in notice_models if n]
-                    notice_models = self.notice_repo.create_all(notice_models)
-                    models += notice_models
+                        notices = await self.notice_crawler.scrape_detail_async(urls[st:ed])
+                        notices = map(
+                            lambda n: add_info(n, department=department, category=category),
+                            notices,
+                        )
+                        notices = await self.notice_embedder.embed_dtos_batch_async(
+                            dtos=list(notices), batch_size=interval
+                        )
 
-                    pbar.update(interval)
+                        notice_models = [self.dto2orm(n) for n in notices]
+                        notice_models = [n for n in notice_models if n]
+                        notice_models = self.notice_repo.create_all(notice_models)
+                        models += notice_models
 
-                    await asyncio.sleep(kwargs.get('delay', 0))
-        except TimeoutError as e:
-            affected = self.notice_repo.delete_by_department(department)
-            logger.exception(
-                f"[{department}] 크롤링에 실패하여 이전 데이터를 초기화 했습니다. ({affected} row deleted)"
-            )
-            raise e
+                        await asyncio.sleep(kwargs.get('delay', 0))
 
-        return models
+                        pbar.update(len(notice_models))
+
+            except TimeoutError as e:
+                #affected = self.notice_repo.delete_by_department(department)
+                logger(f"[{department}-{category}] 크롤링에 실패하였습니다.", level=logging.ERROR)
+                raise e
+
+        return [self.orm2dto(orm) for orm in models]
