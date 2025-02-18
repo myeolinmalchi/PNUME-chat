c82123a641ed4f86adac8190a4861dacb5143bcd
myeolinmalchi <rkd2274@pusan.ac.kr>
2025-02-18 15:20:27 +0900

    feat: `services.notice` 진입점 수정

--- backend/services/notice/__init__.py
+++ backend/services/notice/__init__.py
@@ -1,29 +1,9 @@
-from typing import Literal
 from .dto import *
 from .service import *
 from .embedder import *
-from .crawler import *
+from .crawler.default import *
+from .crawler.me import *
 
-
-def create_notice_service(_type: Literal["default", "me"] = "default"):
-    semester_repo = SemesterRepository()
-    notice_repo = NoticeRepository()
-    notice_embedder = NoticeEmbedder()
-    univ_repo = UniversityRepository()
-    notice_crawler = NoticeCrawler()
-
-    match _type:
-        case "default":
-            Service = NoticeService
-        case "me":
-            Service = NoticeMEService
-
-    notice_service = NoticeService(
-        notice_repo=notice_repo,
-        notice_embedder=notice_embedder,
-        notice_crawler=notice_crawler,
-        university_repo=univ_repo,
-        semester_repo=semester_repo
-    )
-
-    return notice_service
+from .service.base import *
+from .service.default import *
+from .service.me import *
--- backend/services/notice/crawler/__init__.py
+++
@@ -1,3 +0,0 @@
-from .me import *
-from .base import *
-from .default import *
